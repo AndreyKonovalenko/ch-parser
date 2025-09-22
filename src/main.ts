8371adeea8eb481031b0019f55cdfdd9f8eea8c2
Andrey Konovalneko <andreinew@mail.ru>
2025-09-22 10:14:59 +0300

    chore: basic ts setup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-const path = require('path')
-const fs = require('fs')
-const os = require('os')
-const { XMLParser } = require('fast-xml-parser');
-require('dotenv').config()
+import path from  'path';
+import fs from 'fs';
+import os from 'os'
+import { XMLParser } from 'fast-xml-parser';
+import 'dotenv/config'
 
-function parseDate(dateString){
-  dateString = dateString.toString()
-  dateString = dateString.length === 8 ?dateString : 0 + dateString
-  let day = parseInt(dateString.substring(0, 2), 10);
-  let month = parseInt(dateString.substring(2, 4), 10)
-  let year = parseInt(dateString.substring(4, 8), 10); 
+function parseDate(date: string | number){
+  let dateString = date.toString()
+  dateString = dateString.length == 8 ? dateString : '0' + dateString
+  let day = dateString.substring(0, 2);
+  let month = dateString.substring(2, 4)
+  let year = dateString.substring(4, 8); 
 
-  day = day < 10 ? '0' + day: day;
-  month = month < 10 ? '0' + month : month;
+  day = day.length < 2 ? '0' + day: day;
+  month = month.length < 2 ? '0' + month : month;
 
   return `${day}-${month}-${year}`
 }
 
 const pathname = process.env.DIRECTORY
-
+if (!pathname) {
+  process.exit(0)
+}
 // try {
 //   const filenames = fs.readdirSync(pathname)
 //   filenames.forEach(file=> {
@@ -48,9 +50,10 @@ try {
   if (!loan_uuid){
     console.error('uuid задан не верно!!!')
     console.log('npm run start -- 34 chars')
-    return;
+    process.exit(0)
   }
-  const loan = loans.LOAN.find(loan => loan.UUID === loan_uuid)
+
+  const loan = loans.LOAN.find((loan: { UUID: string; }) => loan.UUID === loan_uuid)
   console.log(loan)
   const arraers = loan.PASTDUE_ARREARS.PASTDUE_ARREAR
   let count = 0;
