3126aa44803cb45c32664e325b62b78fded60e59
Andrey Konovalneko <andreinew@mail.ru>
2025-10-30 14:56:04 +0300

    chore: added sentence option logic

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import overview from './overview';
 import { getFiles } from './xml-loader-from-files';
 import { Table } from 'console-table-printer';
 import { saveToFile } from './xml-loader-from-files';
+import sentence from './sentence';
 import 'dotenv/config';
 
 if (process.argv[2] === 'OVERVIEW' || process.argv[2] === 'OVERVIEW-JSON') {
@@ -35,6 +36,18 @@ if (process.argv[2] === 'ALL' || process.argv[2] === 'ALL-JSON') {
       if (process.argv[2] === 'ALL-JSON') {
         allUUID(true, element);
       }
+    });
+  }
+  if (files && files.length === 0) {
+    console.log('There is no files in provided directory with xml extention!');
+  }
+}
+
+if (process.argv[2] === 'SENTENCE') {
+  const files = getFiles();
+  if (files && files.length > 0) {
+    files.forEach(element => {
+      sentence(element);
     });
   }
   if (files && files.length === 0) {
--- src/sentence.ts
+++ src/sentence.ts
@@ -1,7 +1,8 @@
 import { getData } from './xml-loader-from-files';
-import { getOGRN, getPersonName, removeOOO } from './service';
-import { LOAN_KEYS, LOANS_OVERVIEW } from './types';
+import { getOGRN, getPersonName } from './service';
+import { parseDate, pastdueArrearsHandler } from './service';
 import 'dotenv/config';
+import { LOAN_KEYS, STATUS, TYPE, RELATIONSHIP, LOANS_OVERVIEW } from './types';
 
 const sentence = (pathToFile: string) => {
   const jsonObj = getData(pathToFile);
@@ -17,12 +18,16 @@ const sentence = (pathToFile: string) => {
     }
   }
 
-  const hasCurrentPASTDUE = (loans: Array<{ [key: string]: number }>) => {
-    const pastdudedLoan = loans.find(element => element.DELQ_BALANCE > 0);
-    return pastdudedLoan ? 'да' : 'нет';
-  };
+  // const hasCurrentPASTDUE = (loans: Array<{ [key: string]: number }>) => {
+  //   const pastdudedLoan = loans.find(element => element.DELQ_BALANCE > 0);
+  //   return pastdudedLoan ? 'да' : 'нет';
+  // };
 
-  
+  console.log(
+    business ? business.SHORT_NAME : name,
+    business ? business.OGRN : '',
+  );
+  let sum = 0;
   for (const index in loans.LOAN) {
     const loan = loans.LOAN[index];
     const arraers = loan.PASTDUE_ARREARS
@@ -30,27 +35,97 @@ const sentence = (pathToFile: string) => {
         ? loan.PASTDUE_ARREARS.PASTDUE_ARREAR
         : null
       : null;
-      if (arraers && arraers.length > 0) {
-  }
+    if (arraers && arraers.length > 0) {
+      const data = pastdueArrearsHandler(arraers);
+      if (data.length > 0) {
+        const status: keyof typeof STATUS = loan.STATUS;
+        const type: keyof typeof TYPE = loan.TYPE;
+        const relationship: keyof typeof RELATIONSHIP = loan.RELATIONSHIP;
+        const creditLimit = loan.CREDIT_LIMIT;
+        const minPastDueDate = data
+          .map(element => element[LOAN_KEYS.PAST_DUE_DATE])
+          .reduce((accumulator, currentValue) => {
+            if (!currentValue) {
+              return accumulator;
+            }
+            if (!accumulator) {
+              return '9999-12-31';
+            }
+            return accumulator < currentValue ? accumulator : currentValue;
+          });
+        const maxCalculationDate = data
+          .map(element => element[LOAN_KEYS.CALCULATION_DATE])
+          .reduce((accumulator, currentValue) => {
+            if (!currentValue) {
+              return accumulator;
+            }
+            if (!accumulator) {
+              return '1000-01-01';
+            }
+            return accumulator > currentValue ? accumulator : currentValue;
+          });
+        const maxPastDue = data
+          .map(element => element[LOAN_KEYS.PAST_DUE])
+          .reduce((accumulator, currentValue) => {
+            if (!currentValue) {
+              return accumulator;
+            }
+            if (!accumulator) {
+              return 0;
+            }
+            return accumulator > currentValue ? accumulator : currentValue;
+          });
+        const maxDaysPastDue = data
+          .map(element => element[LOAN_KEYS.DAYS_PAST_DUE])
+          .reduce((accumulator, currentValue) => {
+            if (!currentValue) {
+              return accumulator;
+            }
+            if (!accumulator) {
+              return 0;
+            }
+            return accumulator > currentValue ? accumulator : currentValue;
+          });
 
-  const result = {
-    ['наименование']: business
-      ? removeOOO(business.SHORT_NAME)
-      : name
-        ? name
-        : 'не указано',
-    ['ОГРН']: business ? business.OGRN : undefined,
-    loans: [];
-    [LOANS_OVERVIEW.LOANS_ACTIVE]: loansOverveiew.LOANS_ACTIVE,
-    [LOANS_OVERVIEW.PAY_LOAD]: loansOverveiew.PAY_LOAD,
-    [LOAN_KEYS.DELQ_BALANCE]: loans ? hasCurrentPASTDUE(loans.LOAN) : 'нет ки',
-    [LOANS_OVERVIEW.TTL_DELQ_5]: loansOverveiew.TTL_DELQ_5,
-    [LOANS_OVERVIEW.TTL_DELQ_5_29]: loansOverveiew.TTL_DELQ_5_29,
-    [LOANS_OVERVIEW.TTL_DELQ_30_59]: loansOverveiew.TTL_DELQ_30_59,
-    [LOANS_OVERVIEW.TTL_DELQ_60_89]: loansOverveiew.TTL_DELQ_60_89,
-    [LOANS_OVERVIEW.TTL_DELQ_90_PLUS]: loansOverveiew.TTL_DELQ_90_PLUS,
-  };
-  return result;
+        const ttl_5 =
+          loan.TTL_DELQ_5 > 0
+            ? `${[LOANS_OVERVIEW.TTL_DELQ_5]}: ${loan.TTL_DELQ_5} `
+            : '';
+        const ttl_5_29 =
+          loan.TTL_DELQ_5_29 > 0
+            ? `${[LOANS_OVERVIEW.TTL_DELQ_5_29]}: ${loan.TTL_DELQ_5_29} `
+            : '';
+        const ttl_30_59 =
+          loan.TTL_DELQ_30_59 > 0
+            ? `${[LOANS_OVERVIEW.TTL_DELQ_30_59]}: ${loan.TTL_DELQ_30_59} `
+            : '';
+        const ttl_60_89 =
+          loan.TTL_DELQ_60_89 > 0
+            ? `${[LOANS_OVERVIEW.TTL_DELQ_60_89]}: ${loan.TTL_DELQ_60_89} `
+            : '';
+        const ttl_90_plus =
+          loan.TTL_DELQ_90_PLUS > 0
+            ? `${[LOANS_OVERVIEW.TTL_DELQ_90_PLUS]}: ${loan.TTL_DELQ_90_PLUS}`
+            : '';
+        const delq =
+          ttl_5 +
+          ttl_5_29 +
+          ttl_30_59 +
+          ttl_60_89 +
+          ttl_90_plus +
+          `макс дней ${maxDaysPastDue}, макс сумма просрочки ${maxPastDue}`;
+        const result =
+          `uuid: ${loan.UUID} ${STATUS[status]}${loan.FACT_CLOSE_DATE ? ' ' + parseDate(loan.FACT_CLOSE_DATE) + ' ' : ' '}${TYPE[type]} ${RELATIONSHIP[relationship]}` +
+          '\n' +
+          `лимит: ${creditLimit} в период с ${minPastDueDate} по ${maxCalculationDate} было допущено просрочек: ` +
+          '\n' +
+          `${delq}${loan.DELQ_BALANCE > 0 ? ' текущая просрочка ' + loan.DELQ_BALANCE : ''}`;
+        console.log(result);
+        sum++;
+      }
+    }
+  }
+  console.log('всего', sum);
 };
 
 export default sentence;
